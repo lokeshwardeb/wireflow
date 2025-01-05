8e8cecafe260d3cc216bb1c9ee7999bfbb2be608
Lokeshwar Deb <lokeshwardebdevelop@gmail.com>
2025-01-06 02:42:31 +0600

    added the datatable js library and all the other features and functionalities on the software. Now working with the employee info table on the dashboard

--- controllers/controllers.php
+++ controllers/controllers.php
@@ -32,9 +32,14 @@ class controllers extends models
             // that means the user has submitted the form
             $emp_name = $this->pure_data($_POST['emp_name']);
             $emp_age = $this->pure_data($_POST['emp_age']);
+            $emp_mobile_no = $this->pure_data($_POST['emp_mobile_no']);
+            $emp_address = $this->pure_data($_POST['emp_address']);
+            $emp_job_role = $this->pure_data($_POST['emp_job_role']);
+            $emp_salary_structure = $this->pure_data($_POST['emp_salary_structure']);
+            $emp_assigned_shifts = $this->pure_data($_POST['emp_assigned_shifts']);
 
-            if($emp_name == '' || $emp_age){
-                // that means the employee name is blank and it should through an error
+            if($emp_name == '' || $emp_age == '' || $emp_age == '' || $emp_mobile_no == '' || $emp_address == '' || $emp_job_role == '' || $emp_salary_structure == '' || $emp_assigned_shifts == '' ){
+                // that means the data is blank and it should through an error
 
                 $info = [
 
@@ -48,6 +53,56 @@ class controllers extends models
                 // return json_encode($info);
                 return;
 
+
+            }
+
+            // that means the data is not blank and it should be continue the insertion process
+
+            // now first check if the data is already exist or not to the software
+            $result_check_data = $this->get_data_where("employees", "`emp_name` = '$emp_name' AND `emp_age` = '$emp_age' AND `emp_mobile_no` = '$emp_mobile_no' AND `emp_address` = '$emp_address' AND `emp_job_role` = '$emp_job_role' AND `emp_salary_structure` = '$emp_salary_structure' AND `emp_assigned_shifts` = '$emp_assigned_shifts'");
+
+
+            if($result_check_data->num_rows > 0){
+                // that means the data is exist and it should be through and error and it should not continue to the process
+                $info = [
+                    "status" => "error",
+                    "msg" => "The employee already exists to the software !!",
+                ];
+                echo json_encode($info);
+
+                return;
+            }
+
+
+
+            // if the employee details is not exists to the software then continue the process and add it to the software
+
+            $result_add_employee = $this->insert("employees", "`emp_name`, `emp_age`, `emp_mobile_no`, `emp_address`, `emp_job_role`, `emp_salary_structure`, `emp_assigned_shifts`", "'$emp_name', '$emp_age', '$emp_mobile_no', '$emp_address', '$emp_job_role', '$emp_salary_structure', '$emp_assigned_shifts'");
+
+            if($result_add_employee){
+                // that means the employee added successfully
+                
+                $info = [
+
+                    "status" => "success",
+                    "msg" => "The employee has been added successfully !!",
+
+                ];
+                
+                echo json_encode($info);
+
+                return;
+
+            }else{
+                // that means there was something error while adding the employee to the software
+                $info = [
+                    "status" => "error",
+                    "msg" => "There was something error while adding the employee info to the software !!",
+                ];
+
+                echo json_encode($info);
+
+                return;
 
             }
 
--- views/pages/views.dashboard.php
+++ views/pages/views.dashboard.php
@@ -27,12 +27,31 @@ $controllers = new controllers;
     <!-- bootstrap css files -->
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
 
+    <!-- https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css
+    https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css -->
+
+    <!-- <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css"> -->
+
+
+
     <!-- custom css files -->
     <link rel="stylesheet" href="/assets/css/style.css">
+
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="/assets/js/alert.js"></script>
 
     <script src="/assets/js/jquery_3.7.1_min.js"></script>
+
+    <!-- https://code.jquery.com/jquery-3.7.1.js -->
+    <!-- https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js -->
+
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
+
+
+    <!-- https://cdn.datatables.net/2.1.8/js/dataTables.js
+https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js -->
+
     <script src="/assets/js/scripts.js"></script>
 
 
@@ -42,9 +61,9 @@ $controllers = new controllers;
 
     <!-- main code starts here -->
 
-    <script>
+    <!-- <script>
         success_alert("the check", "alert runs");
-    </script>
+    </script> -->
 
     <main>
         <div class="main_section">
@@ -121,6 +140,74 @@ $controllers = new controllers;
                                         require_once __DIR__ . '/inc/_add_employee_component.php';
 
                                         ?>
+
+
+
+                                        <div class="dashboard_employee_table_section mt-4 pt-4 ">
+                                            <div class="container">
+                                                <div class="dashboard_employee_table">
+                                                    <table class="table" id="employee_info_table">
+                                                        <thead>
+                                                            <tr>
+                                                                <th scope="col">#</th>
+                                                                <th scope="col">Employee name</th>
+                                                                <th scope="col">Age</th>
+                                                                <th scope="col">Mobile No</th>
+                                                                <th scope="col">Job Role</th>
+                                                                <th scope="col">Handle</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <?php
+
+                                                            $result_get_employee_data = $controllers->get_data("employees");
+
+                                                            if ($result_get_employee_data) {
+                                                                if ($result_get_employee_data->num_rows > 0) {
+                                                                    // that means the table is not blank
+                                                            
+                                                                    $sl_no = 1;
+
+                                                                    while ($row_emp_data = $result_get_employee_data->fetch_assoc()) {
+                                                                        echo '
+                                                                                <tr class="hover_table" >
+                                                                                    <th scope="row">'. $sl_no .'</th>
+                                                                                    <td>'. $row_emp_data['emp_name'] .'</td>
+                                                                                    <td>'. $row_emp_data['emp_age'] .'</td>
+                                                                                    <td>'. $row_emp_data['emp_mobile_no'] .'</td>
+                                                                                    <td>'. $row_emp_data['emp_job_role'] .'</td>
+                                                                                    <td></td>
+                                                                                    
+                                                                                </tr>
+                                                                            ';
+                                                                            $sl_no++;
+                                                                    }
+
+                                                                }
+                                                            }
+
+
+
+                                                            ?>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+
+                                                <div class="check_table">
+                                                    <?php
+
+                                                    // require_once __DIR__ . '/inc/_employee_table_datatable_ex_template.php';
+                                                    
+                                                    ?>
+                                                </div>
+
+                                            </div>
+                                        </div>
+
+
+
+
+
 
                                     </div>
                                 </div>
